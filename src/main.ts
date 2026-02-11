9e2c497dd087a0e05e5560e7524f77696453e1e8
leobourlot <142550143+leobourlot@users.noreply.github.com>
2026-02-11 00:04:22 -0300

    Bind app listener to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 Backend corriendo en http://localhost:${port}`);
   console.log(`📁 Uploads disponibles en http://localhost:${port}/uploads`);
   
